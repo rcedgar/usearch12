018f335674e6ac7b871bad09ae1a38330e7df3d5
Robert Edgar <robert@drive5.com>
2024-01-30 17:37:23 -0800

    WIP

--- combo.h
+++
@@ -1,9 +0,0 @@
-#ifndef combo_h
-#define combo_h
-
-typedef void (*ON_SET)(const vector<unsigned> &v);
-typedef vector<unsigned>::const_iterator vit;
-
-void EnumPowerSetPerms(unsigned N, ON_SET OnPerm);
-
-#endif // combo_h
